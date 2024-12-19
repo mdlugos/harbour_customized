48f041832a39c66f6e904c56beb29b9ab4f6f828
Marek Dlugosz <ID+USERNAME@users.noreply.github.com>
2024-12-19 08:56:38 +0100

    mingw64

--- hg.bat
+++ hg.bat
@@ -1,5 +1,6 @@
-set path=%~dp0..\h\comp\mingw\bin;%~dp0bin\win\mingw;%~dp0..\tools;%~dp0..\ng;%path%
-set HB_WITH_MYSQL=%~dp0..\MySQL\include
+set path=%~dp0..\h\comp\mingw64\bin;%~dp0bin\win\mingw64;%~dp0..\tools;%~dp0..\ng;%~dp0..\MySQL.x64\LIB;%~dp0..\ads120\acesdk;%~dp0..\Firebird.x64\;%path%
+set HB_WITH_MYSQL=%~dp0..\MySQL.x64\include
 set HB_WITH_ADS=%~dp0..\ads120\acesdk
-set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
+set HB_WITH_FIREBIRD=%~dp0..\Firebird.x64\include
+set HB_WITH_QT=%~dp0..\h\comp\mingw64\include
 rem set HB_BUILD_DEBUG=yes
---
+++ hg32.bat
@@ -0,0 +1,5 @@
+set path=%~dp0..\h\comp\mingw\bin;%~dp0bin\win\mingw;%~dp0..\tools;%~dp0..\ng;%path%
+set HB_WITH_MYSQL=%~dp0..\MySQL\include
+set HB_WITH_ADS=%~dp0..\ads120\acesdk
+set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
+rem set HB_BUILD_DEBUG=yes
