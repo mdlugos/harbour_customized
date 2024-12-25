19668585fa939a694ad0280269dea8ea8fd8f734
Marek Dlugosz <ID+USERNAME@users.noreply.github.com>
2024-12-25 14:18:25 +0100

    bcc

--- h.bat
+++ h.bat
@@ -1,4 +1,4 @@
-set path=%~dp0..\h\COMP\bcc55\bin;%systemroot%;%systemroot%\system32;%~dp0bin\win\bcc;%~dp0..\tools;%~dp0..\ng;%~dp0..\MySQL\LIB;%~dp0..\Firebird
+set path=%~dp0..\h\COMP\bcc55\bin;%~dp0bin\win\bcc;%~dp0..\tools;%~dp0..\ng;%~dp0..\MySQL\LIB;%~dp0..\Firebird;%path%
 set HB_WITH_MYSQL=%~dp0..\MySQL\include
 set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
 SET HB_WITH_ADS=%~dp0..\ads101\acesdk
