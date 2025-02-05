b871f89fc39901ef7b8a351c5b0cfd149a0f1330
Marek Dlugosz <ID+USERNAME@users.noreply.github.com>
2025-02-05 08:59:36 +0100

    hb

--- hb.bat
+++ hb.bat
@@ -3,4 +3,5 @@ set path=%~dp0;%~dp0..\tools;%~dp0..\ads120\acesdk;%~dp0..\MySQL\LIB;%~dp0..\Fir
 set HB_WITH_MYSQL=%~dp0..\MySQL\include
 set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
 SET HB_WITH_ADS=%~dp0..\ads120\acesdk
+set HB_SHELL=nt
 set path=%~dp0..\openssl-3\x86\bin;%path%
