a9e1c3517576ebc40eb33891f200edfa559ec77c
Marek Długosz <ID+USERNAME@users.noreply.github.com>
2025-02-08 15:17:02 +0100

    VC2010

---
+++ vc10.bat
@@ -0,0 +1,17 @@
+SET Path=%~dp0bin\win\msvc;%path%
+set path=%~dp0;%~dp0..\tools;%~dp0..\ads120\acesdk;%~dp0..\MySQL\LIB;%~dp0..\Firebird;%path%
+set HB_WITH_MYSQL=%~dp0..\MySQL\include
+set HB_WITH_ADS=%~dp0..\ads111\acesdk
+set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
+rem set HB_BUILD_DEBUG=yes
+rem # to enable additional system specific logging output,
+rem # OutputDebugString() on Windows, syslog() on *nix systems:
+rem https://github.com/harbour/core/tree/master?tab=readme-ov-file#debugging-options
+rem set HB_TR_LEVEL=debug
+rem set HB_TR_OUTPUT=tracelog.txt
+rem set HB_TR_SYSOUT=yes
+
+SET INCLUDE=%~dp0..\h\comp\VC10\include
+SET LIB=%~dp0..\h\comp\VC10\lib
+SET Path=%~dp0..\h\comp\VC10\bin;%path%
+
