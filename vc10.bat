2f26d99e9cd3d4306ad50fd63de5b5d0ccefce65
Marek Długosz <ID+USERNAME@users.noreply.github.com>
2025-03-29 09:02:59 +0100

    bat

--- vc10.bat
+++ vc10.bat
@@ -1,7 +1,7 @@
 SET Path=%~dp0bin\win\msvc;%path%
-set path=%~dp0;%~dp0..\tools;%~dp0..\ads120\acesdk;%~dp0..\MySQL\LIB;%~dp0..\Firebird;%path%
+set path=%~dp0;%~dp0..\tools;%~dp0..\ads101\acesdk;%~dp0..\MySQL\LIB;%~dp0..\Firebird;%path%
 set HB_WITH_MYSQL=%~dp0..\MySQL\include
-set HB_WITH_ADS=%~dp0..\ads111\acesdk
+set HB_WITH_ADS=%~dp0..\ads101\acesdk
 set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
 rem set HB_BUILD_DEBUG=yes
 rem # to enable additional system specific logging output,
