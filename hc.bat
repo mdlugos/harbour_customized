2dda2a249f988a7078ba160d358150334f7eaa47
Marek Długosz <ID+USERNAME@users.noreply.github.com>
2025-08-27 11:07:34 +0200

    hc

--- hc.bat
+++ hc.bat
@@ -1,6 +1,5 @@
-set path=%~dp0..\h\COMP\clang\bin;%~dp0bin\win\clang;%path%
+set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin;%~dp0bin\win\clang;%path%
 set path=%~dp0;%~dp0..\tools;%~dp0..\ads120\acesdk;%~dp0..\MySQL.x64\LIB;%~dp0..\Firebird.x64\;%path%
 set HB_WITH_MYSQL=%~dp0..\MySQL.x64\include
 set HB_WITH_ADS=%~dp0..\ads120\acesdk
 set HB_WITH_FIREBIRD=%~dp0..\Firebird.x64\include
-set path=%~dp0..\openssl-3\x64\bin;%path%
