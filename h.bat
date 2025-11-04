3f076f752fb0261c7979b1cc15c2aa7eda14d3e4
Marek Długosz <ID+USERNAME@users.noreply.github.com>
2025-11-04 12:05:47 +0100

    DOS

--- h.bat
+++ h.bat
@@ -1,5 +1,7 @@
-cd >cd.txt
-copy ..\set.tx+cd.txt setdbgp.bat
-echo SET HB_DBG_PATH=%%CD%%;%%CD%%\..\common>>setdbgp.bat
+del cd.txt
+del setdbgp.bat
+cd > cd.txt
+copy ..\set.tx + cd.txt setdbgp.bat
+echo SET HB_DBG_PATH=%%CD%%;%%CD%%\..\common >> setdbgp.bat
 hbmk2 @prg -u+head/%1 %2 %3 %4 %5 %6 %7 %8 %9
 setdbgp.bat
