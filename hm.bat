d1e140452015132f261acc7a34b145f74b4a9b8a
Marek Długosz <ID+USERNAME@users.noreply.github.com>
2025-11-10 11:32:38 +0100

    msvc64

--- hm.bat
+++ hm.bat
@@ -1,5 +1,5 @@
 SET Path=%~dp0bin\win\msvc64;%path%
-set path=%~dp0;%~dp0..\tools;%~dp0..\ads120\acesdk;%~dp0..\MySQL.x64\LIB;%~dp0..\Firebird.x64\;%path%
+set path=%~dp0;%~dp0..\tools;%~dp0..\curl\AMD64;%~dp0..\OpenSSL\bin;%~dp0..\ads120\acesdk;%~dp0..\MySQL.x64\LIB;%~dp0..\Firebird.x64\;%path%
 set HB_WITH_MYSQL=%~dp0..\MySQL.x64\include
 set HB_WITH_ADS=%~dp0..\ads120\acesdk
 set HB_WITH_FIREBIRD=%~dp0..\Firebird.x64\include
