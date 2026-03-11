7fd5abe42de20cc48a8d4d18637ea231fa81551c
Marek Dlugosz <ID+USERNAME@users.noreply.github.com>
2026-03-11 18:08:04 +0100

    hb

--- hb.bat
+++ hb.bat
@@ -1,7 +1,8 @@
 set path=%~dp0..\h\COMP\bcc55\bin;%~dp0bin\win\bcc;%path%
 set path=%~dp0;%~dp0..\tools;%~dp0..\ads101\acesdk;%~dp0..\MySQL\LIB;%~dp0..\Firebird;%path%
-set HB_WITH_MYSQL=%~dp0..\MySQL\include
-set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
-SET HB_WITH_ADS=%~dp0..\ads101\acesdk
-set path=%~dp0..\openssl-3\x86\bin;%path%
+rem set HB_WITH_MYSQL=%~dp0..\MySQL\include
+rem set HB_WITH_FIREBIRD=%~dp0..\Firebird\include
+rem SET HB_WITH_ADS=%~dp0..\ads101\acesdk
+set path=%~dp0..\h\comp\mingw\bin;%path%
 rem set HB_MT=yes
+set HB_COMPILER=bcc
