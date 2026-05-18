5c4ccfe59b6115b44b61c6fdf46ef4a868bca5c2
Marek Dlugosz <ID+USERNAME@users.noreply.github.com>
2026-05-18 11:30:15 +0200

    copy /b

--- h.bat
+++ h.bat
@@ -1,7 +1,7 @@
 del cd.txt
 del setdbgp.bat
 cd > cd.txt
-copy ..\set.tx + cd.txt setdbgp.bat
+copy /b ..\set.tx + cd.txt setdbgp.bat
 echo SET HB_DBG_PATH=%%CD%%;%%CD%%\..\common >> setdbgp.bat
 hbmk2 @prg -u+head/%1 %2 %3 %4 %5 %6 %7 %8 %9
 setdbgp.bat
